8a3d2a0dd679f419127fdb7e691f50ae26b07179
iFix <33837893+Back0ldor@users.noreply.github.com>
2019-10-28 14:38:51 +0100

    Update to works on latest libnx api

--- source/main.c
+++ source/main.c
@@ -4,16 +4,15 @@
 #include "menu.h"
 
 int main(int argc, char **argv) {
-    gfxInitDefault();
     consoleInit(NULL);
 
     if (checkRequirement() == 0) {
         printf(CONSOLE_ESC(35;1m)
-            "Welcome to IPSwitch[ReiNX MOD]. Do no evil.\n\n" CONSOLE_ESC(m));
+            "Welcome to IPSwitchMOD. Do no evil.\n\n" CONSOLE_ESC(m));
 
         mainMenu();
     }
 
-    gfxExit();
+    consoleExit(NULL);
     return 0;
 }
