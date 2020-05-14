0abc3610ebe74a8ed02d75893c52d9670dfc410f
Crusty ★ <59363047+CrustySean@users.noreply.github.com>
2020-05-14 13:54:24 +0200

    jp support

--- source/main.c
+++ source/main.c
@@ -8,7 +8,7 @@ int main(int argc, char **argv) {
 
     if (checkRequirement() == 0) {
         printf(CONSOLE_ESC(35;1m)
-            "Welcome to IPSwitchMOD. Do no evil.\n\n" CONSOLE_ESC(m));
+            "IPSwitchへようこそ。 悪をしないでください。\n\n" CONSOLE_ESC(m));
 
         mainMenu();
     }
